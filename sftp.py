21d53a92bb30de1f76a21454558dff330e25ccab
Felipe-dot <felipeluis610@gmail.com>
2022-06-30 16:30:20 -0300

    Further improving my sftp client

--- sftp.py
+++ sftp.py
@@ -1,4 +1,5 @@
 import paramiko
+import os
 paramiko.util.log_to_file("paramiko.log")
 
 # Abrindo uma sessão ssh
@@ -16,81 +17,120 @@ sftp = paramiko.SFTPClient.from_transport(transport)
 
 def get(sftp):
     print("Baixando arquivo...")
-    filepath = "/home/administrador/sftp/teste.txt"
-    localpath = "/home/felipe/SFTP-Client/teste.txt"
-    sftp.get(filepath, localpath)
-    print("Arquivo baixado")
+    filepath = input("Digite o caminho do arquivo no servidor:")
+    localpath = input("Digite o caminho local para armazenar o arquivo:")
+    try:
+        sftp.get(filepath, localpath)
+        print("Arquivo baixado")
+    except:
+        print("Nome do diretório inválido")
 
 def put(sftp):
     print("Atualizando arquivo...")
-    filepath = "/home/administrador/sftp/teste.txt"
-    localpath = "/home/felipe/Documentos/sftp/testando.txt"
-    sftp.put(localpath,filepath)
-    print("Arquivo atualizado")
+    filepath = input("Digite o caminho do arquivo no servidor:")
+    localpath = input("Digite o caminho local do arquivo:")
+    try:
+        sftp.put(localpath,filepath)
+        print("Arquivo atualizado")
+    except:
+        print("Nome do diretório inválido")
 
 def rename(sftp):
     print("Mudando o nome do arquivo...")
-    oldPath = "/home/administrador/sftp/meuNome.txt"
-    newPath = "/home/administrador/sftp/meuNovoNome.txt"
-    sftp.rename(oldPath,newPath)
-    print("Nome do arquivo mudado")
+    oldPath = input("Digite o caminho do arquivo no servidor:")
+    newPath = input("Digite o novo caminho do arquivo no servidor:")
+    try:
+        sftp.rename(oldPath,newPath)
+        print("Nome do arquivo mudado")
+    except: 
+        print("Nome do diretório inválido")
 
 def chmod(sftp):
     print("Mudando o permissionamento do arquivo!")
-    path = "/home/administrador/sftp/teste.sh"
-    sftp.chmod(path,0o111)
-    print("Permissionamento mudado")
+    path = input("Digite o caminho do arquivo no servidor:")
+    permissionCode = input("Digite o código  binário de permissionamento pro arquivo:")
+    try:
+        sftp.chmod(path,oct(int(permissionCode)))
+        print("Permissionamento mudado")
+    except:
+        print("Erro ao mudar o permissionamento")
 
 def chown(sftp):
      print("Mudando o proprietário e o grupo do arquivo!")
-     path = "/home/administrador/sftp/teste.sh"
-     sftp.chown(path,0o1000,0o1000)
-     print("Proprietário e grupo mudado")
+     path = input("Digite o caminho do arquivo no servidor:")
+     uid = input("Digite o id do usuário no servidor:")  
+     gid = input("Digite o id do grupo no servidor:")
+     try:
+        sftp.chown(path,oct(int(uid)),oct(int(gid)))
+        print("Proprietário e grupo mudado")
+     except:
+        print("Erro ao mudar o proprietário do arquivo")
 
 def stat(sftp):
     print("Obtendo status do arquivo...")
-    path = "/home/administrador/sftp/teste.sh"
-    status = sftp.stat(path)
-    print(status)
+    path = input("Digite o caminho do arquivo no servidor:")
+    try:
+        status = sftp.stat(path)
+        print("______Status do arquivo______")
+        print(status)
+    except:
+        print("Nome do Diretório inválido")
 
 def rmdir(sftp):
     print("Removendo diretório...")
-    path = "/home/administrador/sftp/removendoDiretorio"
-    sftp.rmdir(path)
-    print("Diretório removido")
+    path = input("Digite o caminho do diretório no servidor:")
+    try:
+        sftp.rmdir(path)
+        print("Diretório removido")
+    except:
+        print("Nome do diretório inválido")
 
 def mkdir(sftp):
     print("Diretório sendo criado")
-    path = "/home/administrador/sftp/meuNovoDiretorio"
-    sftp.mkdir(path)
-    print("Diretório criado")
+    path = input("Digite o caminho para o novo diretório no servidor:")
+    try:
+        sftp.mkdir(path)
+        print("Diretório criado")
+    except:
+        print("Nome do diretório inválido")
 
 def chdir(sftp):
     print("Mudando o caminho do diretório atual...")
-    path = "/home/administrador/sftp/mudandoDiretorio"
-    sftp.chdir(path)
-    print("Caminho alterado")
+    path = input("Digite o caminho pro novo diretório no servidor:")
+    try:
+        sftp.chdir(path)
+        pwd = sftp.getcwd()
+        print("Novo caminho:"+ pwd)
+        print("Caminho alterado")
+    except:
+        print("Nome do diretório inválido")
 
 def pwd(sftp):
     print("Pegando o caminho do diretorio atual...")
-    path = "/home/administrador/sftp/"
-    sftp.chdir(path)
-    pwd = sftp.getcwd()
-    print(pwd)
+    try:
+        pwd = sftp.getcwd()
+        print("Diretório atual:"+pwd)
+    except:
+        print("Ocorreu um erro ao pegar o caminho atual")
 
 def ls(sftp):
     print("Retorna a lista de arquivos do diretorio atual")
-    path = "/home/administrador/sftp/"
-    sftp.chdir(path)
-    ls = sftp.listdir()
-    print(ls)
+    try:
+        ls = sftp.listdir()
+        print("____Arquivos presentes no diretório atual____")
+        print(ls)
+    except:
+        print("Ocorreu um erro ao listar os arquivos do diretório atual")
 
 def prename(sftp):
     print("Renomeando o diretório")
-    oldPath = "/home/administrador/sftp/diretorio1"
-    newPath = "/home/administrador/sftp/diretorio2"
-    sftp.posix_rename(oldPath,newPath)
-    print("Diretório renomeado")
+    oldPath = input("Digite o caminho antigo do diretório no servidor")
+    newPath = input("Digite o caminho novo pro diretório no servidor")
+    try:
+        sftp.posix_rename(oldPath,newPath)
+        print("Diretório renomeado")
+    except:
+        print("Nome do diretório inválido")
 
 def help(sftp):
     print("Comandos disponivéis")
@@ -105,7 +145,11 @@ def help(sftp):
     print("pwd \t Pegando o caminho do diretório atual")
     print("ls \t Retorna a lista de arquivos do diretório atual")
     print("prename \t Renomeando um diretório")
+    print("clear \t Limpa o console")
     print("?/ help \t Mostra a lista de comandos disponivéis")
+
+def clear(sftp):
+    os.system('clear')
 
 sftpOpcoes = {
     "get": get,
@@ -122,6 +166,7 @@ sftpOpcoes = {
     "prename": prename, 
     "help": help,
     "?":help,
+    "clear": clear
 }
 
 while True: 
